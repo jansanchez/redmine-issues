945660d38deb772461a4cdd0f46a788a7966ff46
Jan Sanchez <joejansanchez@gmail.com>
2014-07-10 17:37:21 -0500

    Api: Habilitando opciones de programa

--- bin/redmine-issues.js
+++ bin/redmine-issues.js
@@ -45,18 +45,13 @@ function config(configValue) {
   }
 
   if (flag) {
-
     var configuration = new Redmine.FileManager(Redmine.configFile);
-
     configuration.set(key, value);
     configuration.save();
-
     console.log("Set correctly {" + key + " : " + configuration.get(key) + "}");
-
   }else{
     console.log("Please enter a valid configuration value(domain, port, apikey, contenttype).");
   };
-
   process.exit();
 };
 
@@ -64,16 +59,13 @@ function config(configValue) {
 
 var options = {};
 
-/*
-.option('-i, --integer <n>', 'An integer argument')
-.option('-o, --optional [value]', 'An optional value')
-.option('-v, --verbose', 'A value that can be increased')
-*/
-
 program
   .version(require('../package.json').version)
   .usage('[options] <file ...>')
-  .option('-c, --config <key>:<value>', 'Configuration options', config);
+  .option('-c, --config <key>:<value>', 'Configuration options', config)
+  .option('-i, --issue <issue>', 'issue id')
+  .option('-p, --percent <percent>', 'percent')
+  .option('-m, --message <message>', 'the message');
 
 program.on('--help', function(){
   console.log('  Examples:');
@@ -95,131 +87,21 @@ program.on('--help', function(){
 
 program.parse(process.argv);
 
-/*
-program.on('--config', function(){
-  console.log('  --config:');
-  console.log(arguments[0]);
-  console.log(arguments[1]);
-  process.exit();
-});
-*/
-
-// options given, parse them
-
-if (program.obj) {
-  if (exists(program.obj)) {
-    options = JSON.parse(fs.readFileSync(program.obj));
-  } else {
-    options = eval('(' + program.obj + ')');
-  }
-}
-
-// --filename
-
-if (program.path) options.filename = program.path;
-
-// --no-debug
-
-options.compileDebug = program.debug;
-
-
-// --pretty
-
-options.pretty = program.pretty;
-
-// --watch
-
-options.watch = program.watch;
-
-// left-over args are file paths
-
-var files = program.args;
-
-
-//stdin();
-
-//console.log('files: ',files);
-
-/**
- * Compile from stdin.
- */
-
-
 
 /*
-function stdin() {
-  var buf = '';
-
-  console.log('stdin');
-
-  process.stdin.setEncoding('utf8');
-
-  process.stdin.on('data', function(chunk){ buf += chunk; });
-
-  process.stdin.on('end', function(){
-    var output = 'nada';
-    process.stdout.write(output);
-  }).resume();
-  
-  process.on('SIGINT', function() {
-    process.stdout.write('\n');
-    process.stdin.emit('end');
-    process.stdout.write('\n');
-    process.exit();
-  });
-}
+* Program options
 */
 
 
-
-/**
- * Process the given path, compiling the redmine_labs files found.
- * Always walk the subdirectories.
- */
-
-/*
-
-function renderFile(path) {
-  var re = /\.redmine_labs$/;
-  fs.lstat(path, function(err, stat) {
-    if (err) throw err;
-    // Found redmine_labs file
-    if (stat.isFile() && re.test(path)) {
-      fs.readFile(path, 'utf8', function(err, str){
-        if (err) throw err;
-        options.filename = path;
-        var fn = options.client ? redmine_labs.compileClient(str, options) : redmine_labs.compile(str, options);
-        var extname = options.client ? '.js' : '.html';
-        path = path.replace(re, extname);
-        if (program.out) path = join(program.out, basename(path));
-        var dir = resolve(dirname(path));
-        mkdirp(dir, 0755, function(err){
-          if (err) throw err;
-          try {
-            var output = options.client ? fn : fn(options);
-            fs.writeFile(path, output, function(err){
-              if (err) throw err;
-              console.log('  \033[90mrendered \033[36m%s\033[0m', path);
-            });
-          } catch (e) {
-            if (options.watch) {
-              console.error(e.stack || e.message || e);
-            } else {
-              throw e
-            }
-          }
-        });
-      });
-    // Found directory
-    } else if (stat.isDirectory()) {
-      fs.readdir(path, function(err, files) {
-        if (err) throw err;
-        files.map(function(filename) {
-          return path + '/' + filename;
-        }).forEach(renderFile);
-      });
-    }
-  });
+if (program.issue){
+  options.issue = program.issue || 0;
 }
-*/
+
+if (program.percent){
+  options.percent = program.percent || 0;
+}
+
+if (program.message){
+  options.message = program.message || "";
+}
 
