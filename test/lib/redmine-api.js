d52694bba82d2dd3aa060911bfb16c749b0c0337
Jan Sanchez <joejansanchez@gmail.com>
2014-07-22 18:45:35 -0500

    Test: Api - guardando información en un issue.

--- test/lib/redmine-api.js
+++ test/lib/redmine-api.js
@@ -9,9 +9,9 @@ describe('Api', function(){
 	beforeEach(function(){
 		api = new Api("config/config.json");
 		options.issue = 43368;
-		options.percent = 10;
-		options.message = "Mensaje";
-		options.estimated = 9;
+		options.percent = 15;
+		options.message = "Mi progreso es: " + options.percent;
+		options.estimated = 0;
 	});
 
 	it('Debe obtener información de un issue.', function(done){
@@ -20,7 +20,17 @@ describe('Api', function(){
 		});
 	});
 
+	it('Debe guardar la información en un issue.', function(done){
+		api.getIssue(options.issue, function(data){
+			api.updateIssue(options.percent, options.message, options.estimated, function(){
+				done();
+			});
+		});
+	});
+
+
 	
-	
+
+
 });
 
